239af5dd8591f639de1cfa5d49211e2c7f25fbd9
amber phillips <amberphillips@Macs-MacBook-Air.local>
2018-03-13 22:33:40 -0500

    revised 4

--- assets/javascript/gif.js
+++ assets/javascript/gif.js
@@ -8,54 +8,94 @@
     //// need to add make sure Gifs are PG
     //// need to add verbiage on what gifs get "rating"
     //// need to make sure gifs can be continous and not stay on the page once another is clicked
-    $("button").on("click", function() {
-    }); 
+  
     
+  
+$("button").on("click", function() {
 
-    var sports = [ football,soccer,tennis,golf,volleyball]
+ 
+  var sport = $(this).attr("data-sport");
 
-    var gif=$(this).attr("data-sport");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sports + "&api_key=dc6zaTOxFJmzC&limit=10"
-    
+  
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    sport + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-
-    $.ajax({
+  $.ajax({
     url: queryURL,
     method: "GET"
   })
+   
+    .done(function(response) {
 
-     .then(function(response) {
-
+    
       var results = response.data;
       console.log(results);
+     
 
       for (var i = 0; i < results.length; i++) {
 
-      if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+    
+        if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+          
+          var sportDiv = $("<div class='item'>");
         
-        var gifDiv = $("<div class='item'>");
+          var rating = results[i].rating;
 
-        
-        var rating = results[i].rating;
+          var p = $("<p>").text("Rating: " + rating);
 
-        var p = $("<p>").text("Rating: " + rating);
+          var sportImage = $("<img>");
 
-      
-        var sportImage = $("<img>");
+          sportImage.addClass('sportImg')
 
-       
-        sportImage.attr("src", results[i].images.fixed_height.url);
+          var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          sport + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-        
-        gifDiv.append(p);
-        gifDiv.append(sportImage);
+          sportImage.attr("src", results[i].images.fixed_height.url);
 
-        // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-        $("#gifs-appear-here").prepend(gifDiv);
-      }
-    }
-});
+          sportImage.attr('data-still', results[i].images.fixed_height_still.url)
+
+          sportImage.attr('data-animate', results[i].images.fixed_height.url)
+
+          sportImage.attr('data-state', 'still');
   
+
+          sportDiv.append(p);
+          sportDiv.append(sportImage);
+          $("#gifs-appear-here").prepend(sportDiv);
+        }
+      }
+    })
+  })
+  
+  
+         $("#gifs-appear-here").on("click",".sportImg", function() {
+        var state = $(this).attr('data-state');
+        if ( state == 'still'){
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        }  
+               else {
+                  $(this).attr('src', $(this).data('still'));
+                  $(this).attr('data-state', 'still');
+              };
+  });
+
+  
+       
+  
+  
+
+
+
+
+          
+    
+
+
+
+
+
+
 
 
 
